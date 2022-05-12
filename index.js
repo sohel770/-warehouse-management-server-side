0eba808268975ebdf49faa7a02d8cda669d09268
sohel Ahamed <sohel770hossain@gmail.com>
2022-05-12 07:40:01 +0600

    commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 const port=process.env.PORT || 5000;
@@ -6,6 +7,25 @@ const port=process.env.PORT || 5000;
 //middleware
 app.use(cors())
 app.use(express.json())
+
+//pass:N6gxhGAANsgZbgm0
+
+const uri = "mongodb+srv://newdata:N6gxhGAANsgZbgm0@cluster0.nf4vh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
+
+
+
+
 
 app.get('/',(req,res) => {
     res.send('running server side')
