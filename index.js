d06b0ed29ed618afe9a5e7ce1cda2bccdd796933
sohel Ahamed <sohel770hossain@gmail.com>
2022-05-12 19:42:36 +0600

    'commit'

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const app = express()
+require('dotenv').config();
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -10,7 +12,7 @@ app.use(express.json())
 
 //pass:N6gxhGAANsgZbgm0
 
-const uri = "mongodb+srv://newdata:N6gxhGAANsgZbgm0@cluster0.nf4vh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nf4vh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -22,13 +24,23 @@ async function run() {
             .collection("products");
             const orderCollection = client.db("farnsOrder").collection("orders");
 
+      //AUTH
+      app.post('/login',async(req,res) => {
+      const user=req.body;
+      const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'1d'
+      })
+      res.send(accessToken)
+      })
+
+
         //getting all the datas
         app.get("/products", async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
-            console.log("CONNECTED TO DB");
+            // console.log("CONNECTED TO DB");
         });
 
 
